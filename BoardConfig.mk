d9378934b01291ff03c80a3390ec8fd17b1a93ad
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 19:56:16 -0700

    kiev: Define paths to DTC and MKDTIMG
    These are no longer on PATH, so we have to define them explicitly
    for the kernel make system to find them.
    
    Change-Id: I69f1a01f2f3d4c90e5a2337f44ee8e1bcb1cc1e8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -60,6 +60,9 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
 TARGET_KERNEL_CONFIG := vendor/kiev_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 # Kernel modules - Audio
 TARGET_MODULE_ALIASES += \
