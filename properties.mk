e0540d6700a280f1cb0cb2d9809016dd244ef281
Park Ju Hyung <qkrwngud825@gmail.com>
2020-01-04 17:48:11 +0900

    racer: Enable automatic refresh rate switching
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>
    Change-Id: I6de04f23d0ad54926a6ad7dff705fd905e4b0c59

--- properties.mk
+++ properties.mk
@@ -20,6 +20,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.brightness_threshold_refresh_rate=6 \
     persist.sys.sf.forbidden_RRR_under_lowbrightness=1
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.set_idle_timer_ms=500 \
+    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_display_power_timer_ms=1000 \
+    ro.surface_flinger.use_smart_90_for_video=true \
+    ro.surface_flinger.refresh_rate_switching=true \
+    ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=1000000
+
 # HDR
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.hdr.config=/vendor/etc/hdr_tm_config.xml
