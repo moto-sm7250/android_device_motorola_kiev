e615665895998ba70c4fe8cd86273beb14212863
Park Ju Hyung <qkrwngud825@gmail.com>
2020-02-16 02:45:58 +0900

    racer: Relax timers for automatic refresh rate switching
    Due to visible gamma shifts on our panels, we should make the switchings
    to happen conservatively.
    
    With idle timer >= touch timer, idle timer now always takes precedence,
    but we're leaving touch timer property to immediately switch back to
    90 Hz on touch input events.
    
    Display power timer is left untouched as it's responsible for display
    mode changes such as Doze.
    
    Change-Id: If8b1e00a2d2213c4d2288ab28fad4ba0826a36e8
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- properties.mk
+++ properties.mk
@@ -21,8 +21,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.forbidden_RRR_under_lowbrightness=1
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.set_idle_timer_ms=500 \
-    ro.surface_flinger.set_touch_timer_ms=750 \
+    ro.surface_flinger.set_idle_timer_ms=4000 \
+    ro.surface_flinger.set_touch_timer_ms=4000 \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.use_smart_90_for_video=true \
     ro.surface_flinger.refresh_rate_switching=true \
