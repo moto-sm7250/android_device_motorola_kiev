fce332cc22d7877d9e1406acf146617da4f58fed
Beta Soft <betaxab@gmail.com>
2021-01-05 17:06:55 +0800

    racer: sort BoardConfig.mk
    - drop TW_EXCLUDE_ENCRYPTED_BACKUPS
    - drop TW_NO_EXFAT_FUSE
    - add TW_INCLUDE_RESETPROP
    - add TW_DEFAULT_BRIGHTNESS
    - add BOARD_PROVIDES_GPTUTILS
    - BOARD_KERNEL_IMAGE_NAME := Image.gz

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,29 @@
 # limitations under the License.
 #
 
-DEVICE_PATH := device/motorola/racer
+# This contains the module build definitions for the hardware-specific
+# components for this device.
+#
+# As much as possible, those components should be built unconditionally,
+# with device-specific names to avoid collisions, to avoid device-specific
+# bitrot and build breakages. Building a component unconditionally does
+# *not* include it on all devices, so it is safe even with hardware-specific
+# components.
+
+PLATFORM_PATH := device/motorola/racer
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := lito
+TARGET_NO_BOOTLOADER := true
+TARGET_USES_UEFI := true
+
+# Platform
+TARGET_BOARD_PLATFORM := lito
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno620
+TARGET_USES_64_BIT_BINDER := true
+TARGET_SUPPORTS_64_BIT_APPS := true
+BUILD_BROKEN_DUP_RULES := true
+QCOM_BOARD_PLATFORMS += lito
 
 # Architecture
 TARGET_ARCH := arm64
@@ -29,12 +51,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a73
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := lito
-TARGET_NO_BOOTLOADER := true
-TARGET_USES_UEFI := true
 
-BUILD_BROKEN_DUP_RULES := true
+# GPT Utils
+BOARD_PROVIDES_GPTUTILS := true
 
 # Kernel
 BOARD_KERNEL_CMDLINE := \
@@ -52,30 +71,34 @@ BOARD_KERNEL_CMDLINE := \
     loop.max_part=7 \
     cgroup.memory=nokmem,nosocket \
     androidboot.boot_devices=soc/1d84000.ufshc \
-    firmware_class.path=/vendor/firmware_mnt/image \
-    androidboot.fastboot=1 \
-    androidboot.selinux=permissive
+    firmware_class.path=/vendor/firmware_mnt/image
+# For the love of all that is holy, please do not include this in your ROM unless you really want TWRP to not work correctly!
+BOARD_KERNEL_CMDLINE += androidboot.fastboot=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOT_HEADER_VERSION := 2
+BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_BASE          := 0x00000000
-BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
 BOARD_KERNEL_OFFSET        := 0x00008000
-BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
 BOARD_RAMDISK_OFFSET       := 0x01000000
+BOARD_KERNEL_SECOND_OFFSET := 0x00f00000
+BOARD_KERNEL_TAGS_OFFSET   := 0x00000100
 BOARD_DTB_OFFSET           := 0x01f00000
+
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
-    DTC=$(OUT_DIR)/../device/motorola/racer/dtc/dtc
+    DTC=$(PLATFORM_PATH)/dtc/dtc
 
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb.img
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image.gz
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+TARGET_PREBUILT_DTB := $(PLATFORM_PATH)/prebuilt/dtb.img
+TARGET_PREBUILT_KERNEL := $(PLATFORM_PATH)/prebuilt/Image.gz
+BOARD_PREBUILT_DTBOIMAGE := $(PLATFORM_PATH)/prebuilt/dtbo.img
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 
+TARGET_KERNEL_VERSION := 4.14
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
-TARGET_KERNEL_VERSION := 4.14
+
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := r328903
 
@@ -89,28 +112,16 @@ BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 
-BOARD_INCLUDE_RECOVERY_DTBO := true
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-
-# Platform
-TARGET_BOARD_PLATFORM := lito
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno620
-TARGET_SUPPORTS_64_BIT_APPS := true
-QCOM_BOARD_PLATFORMS += lito
-
 # Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144
-
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
 BOARD_DTBOIMG_PARTITION_SIZE := 25165824
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 536870912
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 10737418240
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
-TARGET_USERIMAGES_USE_EXT4 := true
-TARGET_USERIMAGES_USE_F2FS := true
 
 # Super
 BOARD_SUPER_PARTITION_SIZE := 8690597888
@@ -121,23 +132,38 @@ BOARD_MOTOROLA_DYNAMIC_PARTITIONS_PARTITION_LIST := \
     product \
     vendor
 
-TARGET_NO_KERNEL := false
-TARGET_NO_RECOVERY := false
-
-# Partitions (listed in the file) to be wiped under recovery.
-# TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery.fstab
-
-# Workaround for error copying vendor files to recovery ramdisk
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_COPY_OUT_PRODUCT := product
-
 # Recovery
-BOARD_HAS_LARGE_FILESYSTEM := true
 BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_LARGE_FILESYSTEM := true
+BOARD_INCLUDE_RECOVERY_DTBO := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
+
+# A/B device flags
+AB_OTA_UPDATER := true
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+BOARD_USES_RECOVERY_AS_BOOT := false
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
+
+# TWRP specific build flags
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+# TW_EXCLUDE_ENCRYPTED_BACKUPS := true
+TW_INCLUDE_NTFS_3G := true
+TW_INCLUDE_RESETPROP := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_BRIGHTNESS := 120
+TW_THEME := portrait_hdpi
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
+TW_NO_SCREEN_BLANK := true
 
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental"
@@ -174,36 +200,32 @@ TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
 # Encryption
 PLATFORM_VERSION := 16.1.0
 PLATFORM_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH :=  2099-12-31
+VENDOR_SECURITY_PATCH := 2099-12-31
 TW_INCLUDE_CRYPTO := true
 BOARD_USES_QCOM_FBE_DECRYPTION := true
 BOARD_USES_METADATA_PARTITION := true
 
-# TWRP specific build flags
-AB_OTA_UPDATER := true
+# Extras
+TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+
 BOARD_SUPPRESS_SECURE_ERASE := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-RECOVERY_SDCARD_ON_DATA := true
-TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel0-backlight/brightness"
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TW_EXCLUDE_ENCRYPTED_BACKUPS := true
-TW_EXTRA_LANGUAGES := true
-TW_HAS_EDL_MODE := true
-TW_INCLUDE_NTFS_3G := true
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_MAX_BRIGHTNESS := 255
-TW_NO_EXFAT_FUSE := true
-TW_NO_SCREEN_BLANK := true
-TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
+TW_HAS_EDL_MODE := true
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := true
+# TW_DEFAULT_LANGUAGE := zh_CN
 
 # Notch Offset
 TW_Y_OFFSET := 89
 TW_H_OFFSET := -89
 
-# TWRP Debug Flags
+# Debug flags
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
+
+ALLOW_MISSING_DEPENDENCIES := true
+# Workaround for error copying vendor files to recovery ramdisk
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
