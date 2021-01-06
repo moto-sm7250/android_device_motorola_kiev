2e34a2440c3fb3e348951ee2ee561a5733c22489
Michael Bestas <mkbestas@gmail.com>
2021-01-06 19:29:05 +0200

    kiev: gps/location: Ignore some build warnings
    Change-Id: If1dfd39ab820cdf5e7ac5caf05dc8fea1cd95235

--- gps/Android.mk
+++ gps/Android.mk
@@ -3,17 +3,18 @@ ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 # Set required flags
 GNSS_CFLAGS := \
     -Werror \
-    -Wno-error=unused-parameter \
+    -Wno-unused-parameter \
     -Wno-error=macro-redefined \
     -Wno-error=reorder \
+    -Wno-reorder-ctor \
     -Wno-error=missing-braces \
     -Wno-error=self-assign \
-    -Wno-error=enum-conversion \
-    -Wno-error=logical-op-parentheses \
+    -Wno-enum-conversion \
+    -Wno-logical-op-parentheses \
     -Wno-error=null-arithmetic \
     -Wno-error=null-conversion \
     -Wno-error=parentheses-equality \
-    -Wno-error=undefined-bool-conversion \
+    -Wno-undefined-bool-conversion \
     -Wno-error=tautological-compare \
     -Wno-error=switch \
     -Wno-error=date-time
