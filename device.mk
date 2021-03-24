2bdb742a4d827b55bbb837b456b15a0e35a8f7a1
Albert Wang <albertccwang@google.com>
2021-03-25 00:03:02 +0800

    kiev: Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling
    
    Bug: 161414036
    Test: Pass USB V1.3 HIDL tests
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: Iffe00f8753206fb66cd3ab96cae5aa5ad9c410cd

--- device.mk
+++ device.mk
@@ -573,7 +573,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.3-service-qti
 
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
