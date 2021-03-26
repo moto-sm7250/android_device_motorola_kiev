8062aa87960c05b429f4c3b69ab3249532953975
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-03-25 20:40:24 -0400

    kiev: Setup sku specific hardware support
    - Add odm manifests for different fingerprint sensor and nfc
      hardware.
    
    Change-Id: Ia3103d8f17d80022699387f2ad34c64c390d7d4e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,5 +33,21 @@ SOONG_CONFIG_MOTOROLA_SM7250_INIT_DEVICE_LIB := //$(DEVICE_PATH):libinit_kiev
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/kiev_defconfig
 
+# ODM
+ODM_MANIFEST_SKUS := \
+    dn \
+    dnj \
+    dnp \
+    n \
+    nj \
+    np
+
+ODM_MANIFEST_DN_FILES := $(DEVICE_PATH)/odm/manifest_dn.xml
+ODM_MANIFEST_DNJ_FILES := $(DEVICE_PATH)/odm/manifest_dnj.xml
+ODM_MANIFEST_DNP_FILES := $(DEVICE_PATH)/odm/manifest_dnp.xml
+ODM_MANIFEST_N_FILES := $(DEVICE_PATH)/odm/manifest_n.xml
+ODM_MANIFEST_NJ_FILES := $(DEVICE_PATH)/odm/manifest_nj.xml
+ODM_MANIFEST_NP_FILES := $(DEVICE_PATH)/odm/manifest_np.xml
+
 # SELinux
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
