6635789c402fe1d207629ab8b05aa17b87281774
Erfan Abdi <erfangplus@gmail.com>
2021-04-16 03:11:08 +0430

    kiev: Add lights hal
    * Based on motorola sdm632
    
    Change-Id: If4fe7a661e532de13b86b80a6dd18c0281172e7d

--- device.mk
+++ device.mk
@@ -362,6 +362,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.kiev
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm \
