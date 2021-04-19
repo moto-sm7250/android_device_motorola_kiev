7d4e326226c8793531a082155167fa28ce4a7ff5
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-04-19 07:57:36 -0400

    kiev: Define paths to DTC_OVERLAY_TEST_EXT
    Change-Id: Ie49852f4650609753bb201a8af0145382db75aff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,6 +62,7 @@ TARGET_KERNEL_CONFIG := vendor/kiev_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
+    DTC_OVERLAY_TEST_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/ufdt_apply_overlay \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
 
 # Kernel modules - Audio
