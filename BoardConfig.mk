f0200caa45d917131aaebb34d59de1f901acff6c
Andrew Hexen <SyberHexen@gmail.com>
2021-07-01 12:15:52 -0700

    kiev: dtc: specify we are using prebuilt.
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,6 +90,7 @@ TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := vendor/kiev_recovery_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/sm7250
 TARGET_KERNEL_ADDITIONAL_FLAGS += \
+    DTC_PREBUILT=true \
     DTC=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/dtc/dtc \
     DTC_OVERLAY_TEST_EXT=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/ufdt_apply_overlay \
     MKDTIMG=$(shell pwd)/prebuilts/misc/$(HOST_OS)-x86/libufdt/mkdtimg
