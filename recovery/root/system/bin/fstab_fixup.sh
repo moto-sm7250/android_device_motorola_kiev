a7360faa95d276c618a86a04b143cddc9cd302b5
Andrew Hexen <SyberHexen@gmail.com>
2021-07-28 06:51:00 -0700

    kiev: make fixup work with 18.1

--- recovery/root/system/bin/fstab_fixup.sh
+++ recovery/root/system/bin/fstab_fixup.sh
@@ -2,7 +2,9 @@
 
 fstab_fixup()
 {
-	sed -i "s|keydirectory=/metadata/vold/metadata_encryption,quota,reservedsize=512M|quota,reservedsize=128M|" /system/etc/recovery.fstab
+	sed -i "s|fsync_mode=nobarrier,inlinecrypt|fsync_mode=nobarrier" /system/etc/recovery.fstab
+	sed -i "s|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,quota,reservedsize=512M|quota,reservedsize=128M|" /system/etc/recovery.fstab
+
 }
 
 fstab_fixup
