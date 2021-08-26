d2859e3f190b8cea1dfd9973801ed6df4fdaa0e7
Andrew Hexen <SyberHexen@gmail.com>
2021-08-26 06:50:58 -0700

    kiev: properties.mk: update device properties
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Iec8ec6b5b1afee2aa9c42c37455ca1ff14803792

--- properties.mk
+++ properties.mk
@@ -20,7 +20,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     af.fast_track_multiplier=2 \
     audio.deep_buffer.media=true \
-    audio.offload.video=true \
+    audio.offload.disable=false \
+    audio.offload.video=false \
+    tunnel.audio.encode=true \
+    qcom.hw.aac.encoder=true \
+    persist.audio.endcall.delay=250 \
     persist.vendor.audio.dualmic.config=endfire \
     persist.vendor.audio.fluence.voicecall=true \
     persist.vendor.audio.fluence.voicecomm=true \
@@ -38,21 +42,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.audio.hw.aac.encoder=true \
     vendor.audio.noisy.broadcast.delay=600 \
     vendor.audio.offload.buffer.size.kb=32 \
-    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.gapless.enabled=false \
     vendor.audio.offload.multiaac.enable=true \
     vendor.audio.offload.multiple.enabled=false \
     vendor.audio.offload.passthrough=false \
     vendor.audio.offload.pstimeout.secs=3 \
     vendor.audio.offload.track.enable=true \
-    vendor.audio.parser.ip.buffer.size=0 \
+    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.snd_card.open.retries=50 \
     vendor.audio.tunnel.encode=false \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio.use.sw.alac.decoder=false \
+    vendor.audio.use.sw.ape.decoder=false \
     vendor.audio_hal.in_period_size=144 \
     vendor.audio_hal.period_multiplier=3 \
-    vendor.audio_hal.period_size=192 \
+    vendor.audio_hal.period_size=240 \
     vendor.voice.path.for.pcm.voip=true
+    use.voice.path.for.pcm.voip=true
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     audio.offload.min.duration.secs=60 \
@@ -105,10 +111,19 @@ PRODUCT_ODM_PROPERTIES += \
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
-    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
-    persist.vendor.qcom.bluetooth.soc=cherokee \
+    net.bt.name='Moto G 5G' \
     ro.bluetooth.a2dp_offload.supported=true \
+    ro.vendor.bluetooth.wipower=false \
+    persist.sys.fflag.override.settings_bluetooth_hearing_aid=true \
+    persist.vendor.bt.aac_vbr_frm_ctl.enabled=true \
+    persist.vendor.btstack.enable.lpa=true \
+    persist.vendor.qcom.bluetooth.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aac-ldac \
+    persist.vendor.qcom.bluetooth.aac_vbr_ctl.enabled=true \
+    persist.vendor.qcom.bluetooth.aptxadaptiver2_1_support=false \
+    persist.vendor.qcom.bluetooth.enable.splita2dp=true \
+    persist.vendor.qcom.bluetooth.scram.enabled=false \
+    persist.vendor.qcom.bluetooth.soc=cherokee \
+    persist.vendor.qcom.bluetooth.twsp_state.enabled=false \
     vendor.qcom.bluetooth.soc=cherokee
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -152,19 +167,23 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.vulkan=adreno \
     ro.hardware.egl=adreno \
-    vendor.display.disable_scaler=0 \
+    vendor.display.comp_mask=0 \
     vendor.display.disable_excl_rect=0 \
     vendor.display.disable_excl_rect_partial_fb=1 \
-    vendor.display.comp_mask=0 \
-    vendor.display.enable_posted_start_dyn=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.display.enable_async_powermode=1  \
     vendor.display.enable_optimize_refresh=1 \
-    vendor.display.use_smooth_motion=1 \
+    vendor.display.enable_posted_start_dyn=1 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_idle_time_hdr=1 \
+    vendor.display.disable_idle_time_video=1 \
     vendor.display.disable_offline_rotator=1 \
-    vendor.display.disable_hw_recovery_dump=1
+    vendor.display.use_smooth_motion=1
 
 # DPM
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.dpm.feature=1
+    persist.vendor.dpm.feature=11 \
+    persist.vendor.dpmhalservice.enable=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -230,12 +249,41 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.hardware.keystore_desede=true
 
+# Low-Memory Killer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.thrashing_limit=60 \
+    ro.lmk.swap_free_low_percentage=10 \
+    ro.lmk.psi_partial_stall_ms=50 \
+    ro.lmk.swap_util_max=90 \
+    ro.lmk.pgscan_limit=3000 \
+    ro.lmk.file_low_percentage=20 \
+    ro.lmk.threshold_decay=50 \
+    ro.config.use_compaction=true \
+    ro.config.compact_action_1=4 \
+    ro.config.compact_action_2=2 \
+    ro.lowmemdetector.psi_low_stall_us=100000 \
+    ro.lowmemdetector.psi_medium_stall_us=150000 \
+    ro.lowmemdetector.psi_high_stall_us=200000
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
     debug.stagefright.ccodec=1 \
     debug.stagefright.omx_default_rank=0 \
+    media.aac_51_output_enabled=true \
+    media.settings.xml=/vendor/etc/media_profiles_vendor.xml \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true \
+    media.stagefright.enable-fma2dp=true \
+    media.stagefright.enable-scan=true \
+    media.recorder.show_manufacturer_and_model=true \
+    mmp.enable.3g2=true \
+    persist.mm.enable.prefetch=true \
     persist.vendor.media.recorder.bt709=true
+    vendor.mm.en.sec.smoothstreaming=false \
+    vendor.mm.enable.qcom_parser=16777215
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     av.offload.enable=true \
@@ -282,39 +330,53 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.build.vendorprefix=/vendor \
-    ro.vendor.radio.imei.sv=11 \
+    ro.vendor.radio.imei.sv=5 \
+    ro.vendor.radio.mbn.load_special_sku=1 \
+    persist.radio.call_type=1 \
+    persist.radio.mode_pref_nv10=1 \
+    persist.radio.multisim.config=dsds \
+    persist.rcs.supported=1 \
+    persist.vendor.eab.supported=1 \
     persist.vendor.data.iwlan.enable=true \
+    persist.vendor.ims.cam_sensor_delay=20 \
+    persist.vendor.ims.display_delay=40 \
+    persist.vendor.ims.playout_delay=50 \
+    persist.vendor.lte.pco_supported=true \
+    persist.vendor.radio.0x9e_not_callname=1 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.aosp_usr_pref_sel=true \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
     persist.vendor.radio.custom_ecc=1 \
     persist.vendor.radio.data_con_rprt=1 \
     persist.vendor.radio.data_ltd_sys_ind=1 \
-    persist.vendor.radio.manual_nw_rej_ct=1 \
-    persist.vendor.radio.no_wait_for_card=1 \
     persist.vendor.radio.dfr_mode_set=1 \
-    persist.vendor.radio.oem_ind_to_both=0 \
-    persist.vendor.radio.qcril_uim_vcc_feature=1 \
-    persist.vendor.radio.0x9e_not_callname=1 \
-    persist.vendor.radio.mt_sms_ack=30 \
-    persist.vendor.radio.force_get_pref=1 \
-    persist.vendor.radio.is_wps_enabled=true \
-    persist.vendor.radio.eri64_as_home=1 \
-    persist.vendor.radio.add_power_save=1 \
-    persist.vendor.radio.msgtunnel.start=true \
-    persist.vendor.radio.lte_vrte_ltd=1 \
-    persist.vendor.radio.sw_mbn_update=0 \
-    persist.vendor.radio.sar_sensor=1 \
-    persist.vendor.radio.fi_supported=0 \
-    persist.vendor.radio.rat_on=other \
-    persist.vendor.radio.hidl_dev_service=1 \
-    persist.vendor.lte.pco_supported=true \
+    persist.vendor.radio.enable_temp_dds=true \
     persist.vendor.radio.enableadvancedscan=true \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
+    persist.vendor.radio.eri64_as_home=1 \
+    persist.vendor.radio.fi_supported=0 \
+    persist.vendor.radio.flexmap_type=none \
+    persist.vendor.radio.force_get_pref=1 \
+    persist.vendor.radio.hidl_dev_service=1 \
+    persist.vendor.radio.is_wps_enabled=true \
+    persist.vendor.radio.jbims=1 \
+    persist.vendor.radio.lte_vrte_ltd=1 \
+    persist.vendor.radio.manual_nw_rej_ct=1 \
+    persist.vendor.radio.msgtunnel.start=true \
+    persist.vendor.radio.mt_sms_ack=30 \
+    persist.vendor.radio.no_wait_for_card=1 \
+    persist.vendor.radio.oem_ind_to_both=0 \
     persist.vendor.radio.procedure_bytes=SKIP \
+    persist.vendor.radio.qcril_uim_vcc_feature=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sar_sensor=1 \
+    persist.vendor.radio.sib16_support=1 \
     persist.vendor.radio.snapshot_timer=22 \
     persist.vendor.radio.snapshot_enabled=1 \
-    persist.vendor.radio.aosp_usr_pref_sel=true \
-    persist.vendor.radio.flexmap_type=none \
-    persist.vendor.radio.enable_temp_dds=true
+    persist.vendor.radio.sw_mbn_update=0 \
+    persist.vendor.radio.start_ota_daemon=0 \
+    persist.vendor.rcs.presence.provision=0 \
+    persist.vendor.vt.supported=1 \
+    wifi.aware.interface=wifi-aware0
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     DEVICE_PROVISIONED=1 \
@@ -324,7 +386,19 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.vendor.use_data_netmgrd=true \
     telephony.lteOnCdmaDevice=1,1
 
-# Sensor
+# RmNet Data
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rmnet.data.enable=true \
+    persist.data.wda.enable=true \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.ul_mode=5 \
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.dev_name=rmnet_usb0
+
+# Sensors
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.sensors.enable.mag_filter=true
 
@@ -350,11 +424,13 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Sensor
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    persist.vendor.sensors.enable.mag_filter=true
+    persist.vendor.sensors.enable.mag_filter=true \
+    persist.vendor.sensors.allow_non_default_discovery=true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.sensors.hal_trigger_ssr=false \
     persist.vendor.sensors.odl.adsp=true \
+    ro.vendor.hw.thermal_skin_val=battery \
     ro.vendor.sensors.glance_approach=false \
     ro.vendor.sensors.mot_ltv=true
 
