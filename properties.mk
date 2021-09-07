de11aa98023561bbfe93bd3f284ffd5eb88dbc45
Andrew Hexen <SyberHexen@gmail.com>
2021-09-07 09:18:23 -0700

    Revert "kiev: properties.mk: enable adb early"
    This reverts commit 7e7c078f4369ee5ca4e43e5ba3f62a0aa2c9af88.
    
    Change-Id: Ia003dccef918b133eb5d076f4c75d5fe402c6887

--- properties.mk
+++ properties.mk
@@ -2,10 +2,6 @@
 # Properties for Kiev
 #
 
-# ADB - Early
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.boot.adb_early=1
-
 # Audio - ACDB
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
