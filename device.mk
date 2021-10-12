4858a120d890c1d0bc95b565a0b50e0450f66424
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 09:34:08 +0200

    kiev: Uprev bluetooth audio HIDL impl to 2.1
    * We get free LC3 support, isn't it poggers?
    
    Change-Id: Ia8cef3905c133441cbe741c83d6d5ecdf4be2338

--- device.mk
+++ device.mk
@@ -126,7 +126,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
-    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
     audioadsprpcd \
     audio.a2dp.default \
     audio.bluetooth.default \
