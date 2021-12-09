aff373a79cedadd4bf092146fe3158c7302621b7
Andrew Hexen <SyberHexen@gmail.com>
2021-12-09 03:41:02 -0800

    kiev: fstab_fixup: Update the methods for A11
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: I417a3ea1f7dc2bca2a5e4414d19652f74502a1d5

--- recovery/root/system/bin/fstab_fixup.sh
+++ recovery/root/system/bin/fstab_fixup.sh
@@ -2,11 +2,30 @@
 
 fstab_fixup()
 {
-	sed -i "s|fsync_mode=nobarrier,inlinecrypt|fsync_mode=nobarrier" /system/etc/recovery.fstab
-	sed -i "s|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,quota,reservedsize=512M|quota,reservedsize=128M|" /system/etc/recovery.fstab
-
+	sed -i "s|fileencryption=aes-256-xts:aes-256-cts:v2+inlinecrypt_optimized,keydirectory=/metadata/vold/metadata_encryption,quota,reservedsize=512M,sysfs_path=/sys/devices/platform/soc/1d84000.ufshc,checkpoint=fs|fileencryption=ice,quota,reservedsize=128M,checkpoint=fs|" /system/etc/recovery.fstab
+	sed -i "/by-name\/metadata/d" /system/etc/recovery.fstab
 }
 
-fstab_fixup
+avb_version()
+{
+	avb_version=""
+	avb_version=$(getprop ro.boot.vbmeta.avb_version)
+	    echo "avb version is: [$avb_version]"
+	    case $avb_version in
+	        1.0)
+		        echo "Android 11 Stock Rom Detected"
+			    fstab_fixup
+			    setprop fbe.metadata.wrappedkey ""
+			    setprop ro.product.first_api_level ""
+	            ;;
+	        *)
+	            echo "Android 11+ ROM Detected"
+		        setprop fbe.metadata.wrappedkey "true"
+		        setprop ro.product.first_api_level "30"
+	            ;;
+	        esac
+}
+
+avb_version
 
 exit 0
