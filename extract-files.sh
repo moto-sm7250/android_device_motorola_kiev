85427b795016ca12d89fb17f897779ca2e000336
dianlujitao <dianlujitao@lineageos.org>
2021-12-23 15:10:00 +0800

    kiev: Move moto-telephony.jar to system_ext and remove unused moto jar
     * moto-telephony.jar is a dependence of ims.apk and thus should locate
       in system_ext as well.
     * The rest jars are not needed as far as I observe.
    
    Change-Id: I0711c0178906293179998eae70663de7b09a4a77

--- extract-files.sh
+++ extract-files.sh
@@ -67,6 +67,9 @@ function blob_fixup() {
     product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml | product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
         sed -i 's/xml version="2.0"/xml version="1.0"/' "${2}"
         ;;
+    system_ext/etc/permissions/moto-telephony.xml)
+        sed -i "s|system|system/system_ext|" "${2}"
+        ;;
     esac
 }
 
