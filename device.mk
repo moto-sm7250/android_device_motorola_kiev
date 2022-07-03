1c31e3949e94d9c8daa0f72fea3bbd18d9333f6e
Andrew Hexen <SyberHexen@gmail.com>
2022-07-03 13:07:22 -0700

    kiev: drop snap
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Idd086693f79c58cb797204bb424bd60c6b2c22bc

--- device.mk
+++ device.mk
@@ -35,7 +35,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
-    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
 
 PRODUCT_PACKAGES += \
 	NoCutoutOverlay
@@ -196,7 +195,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
     libcamera2ndk_vendor \
     libxml2 \
-    Snap \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
