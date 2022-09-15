c1b05867496c498706c0cd816d5b94c981c839ca
Andrew Hexen <SyberHexen@gmail.com>
2022-09-15 04:46:26 -0700

    Revert "kiev: drop sound_trigger"
    This reverts commit 96bef0e0fc6e051961d42e3c6e8cfd02f41e924b.
    
    Signed-off-by: Andrew Hexen <SyberHexen@gmail.com>
    Change-Id: Iee6e2caabdd782adaff57fe772187f5c14b48af6

--- device.mk
+++ device.mk
@@ -127,6 +127,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
+    android.hardware.soundtrigger@2.2-impl \
     android.hardware.bluetooth.audio@2.1-impl \
     audioadsprpcd \
     audio.a2dp.default \
